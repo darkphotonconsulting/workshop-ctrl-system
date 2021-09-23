5582dc9fe4f96db11040856e598fb6efd14774f0
apsamuel <aaron.psamuel@gmail.com>
2021-09-23 13:28:17 -0400

    dev: add settings modules (playing with this)

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+class Config(object):
+    """Configuration Class
+    """
+    raspi_service_host = '0.0.0.0'
+    raspi_service_port = 5001
+    metrics_service_host = '0.0.0.0'
+    metrics_service_port = 5002
+    mongodb_host = '127.0.0.1'
+    mongodb_port = 27017
---
+++ config/settings.py
@@ -0,0 +1,23 @@
+class Config(object):
+    """Configuration Class
+    """
+    raspi_service_host = '0.0.0.0'
+    raspi_service_port = 5001
+    metrics_service_host = '0.0.0.0'
+    metrics_service_port = 5002
+    mongodb_host = '127.0.0.1'
+    mongodb_port = 27017
+    mongodb_database = 'headunit-v1'
+    # database.collection.schema
+    mongodb_schema = {
+        'pi-data': {
+            'pi-system': {
+            },
+            'pi-cpu': {},
+            'pi-mem': {},
+            'pi-io': {},
+            'pi-net': {},
+            'gpio-system': {},
+            'gpio-state': {}
+        }
+    }
