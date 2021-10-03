b2e46017ad6160013c85b3ac38bd1cab212f2829
apsamuel <aaron.psamuel@gmail.com>
2021-10-03 11:29:07 -0400

    feat: update data import script

---
+++ seed-v2.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import json
+import logging
+from pymongo import MongoClient
+from argparse import ArgumentParser
+from pylibs.logging.loginator import Loginator
+program_name = "HeadUnit Data Import & Export Tool"
+parser = ArgumentParser(
+    prog=program_name,
+
+)
+
+
+conn_args = parser.add_argument_group('connection')
+action_args = parser.add_argument_group('actions')
+conn_args.add_argument('--mongo-host',
+                       type=str,
+                       required=False,
+                       action='store',
+                       help='MongoDB host IP or FQDN',
+                       default="mongo.darkphotonworks-labs.io"
+)
+conn_args.add_argument('--mongo-port',
+                       type=int,
+                       required=False,
+                       action='store',
+                       help='MongoDB Port',
+                       default=27017
+)
+conn_args.add_argument('--mongo-username',
+                       type=str,
+                       required=True,
+                       action='store',
+                       help='MongoDB Application Username'
+)
+conn_args.add_argument('--mongo-password',
+                       type=str,
+                       required=True,
+                       action='store',
+                       help='MongoDB Application Password'
+)
