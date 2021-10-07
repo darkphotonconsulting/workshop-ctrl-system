bc92cc62790f0ed8b4e0d38f49bfecc3b39d2c35
apsamuel <aaron.psamuel@gmail.com>
2021-10-07 05:39:26 -0400

    docs: improve docstrings for modules

--- pylibs/constants/__init__.py
+++ pylibs/constants/__init__.py
@@ -1,8 +1,8 @@
 """ pylibs.constants
 
-Common constant values shared across headunit components
+Commonly constant values shared across headunit components
 
 Constants:
-    MONGO_STRUCTURE (dict) - a layout of the default mongodb structure, top level keys are databases and second-level keys map a user parameter (system, gpios, relays) to a collection name
-    FILE_MAP (dict) - maps user parameter to a file name, used when loading, or dumping data to the filesystem
+    pylibs.constants.MONGO_STRUCTURE (dict) - a layout of the default mongodb structure, top level keys are databases and second-level keys map a user parameter (system, gpios, relays) to a collection name
+    pylibs.constants.FILE_MAP (dict) - maps user parameter to a file name, used when loading, or dumping data to the filesystem
 """
--- pylibs/forms/__init__.py
+++ pylibs/forms/__init__.py
@@ -2,7 +2,7 @@
 
 this module provides classes that contain form logic used backend api services such as the raspi-service
 
-Currently the relay form is the only defined and used form
+pylibs.forms.relay - A WTF form for CRUD on the relay object type
 
 TODO: define input forms for other variable hardware types such as additional sensors, you may want to connect to system
 """
