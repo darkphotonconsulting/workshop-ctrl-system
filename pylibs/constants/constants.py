878b21309ee1a747cc949eddf9d00d60dfc41ed0
apsamuel <aaron.psamuel@gmail.com>
2021-10-12 04:46:43 -0400

    feat: improve constants

--- pylibs/constants/constants.py
+++ pylibs/constants/constants.py
@@ -2,7 +2,9 @@
 
 This file contains constants frequently referenced by other components in the headunit ecosystem
 """
-MONGO_STRUCTURE = {
+
+# defines the resultant mongo structure after seed/migration
+MONGO_STRUCTURE: dict = {
     'static': {
         'system': 'static-system',
         'gpios': 'static-gpios',
@@ -15,9 +17,23 @@ MONGO_STRUCTURE = {
         'system_cpu_stats': 'dynamic-system-memory-statistics'
     }
 }
-FILE_MAP = {
+# database names the application knows how to configure
+SUPPORTED_DATABASE_NAMES: list = list(MONGO_STRUCTURE.keys())
+# collection aliases that can be passed into functions that need to lookup the proper schema templates
+COLLECTION_ALIAS_MAP: dict = {k: list(MONGO_STRUCTURE[k].keys()) for k in MONGO_STRUCTURE.keys() }
+# list of supported collection names mapped to their containing databases
+SUPPORTED_COLLECTIONS_MAP: dict = {k: list(MONGO_STRUCTURE[k].values()) for k in MONGO_STRUCTURE.keys() }
+# list of defaults used when writing data to file
+FILE_MAP: dict = {
     'extension': 'json',
     'system': 'system',
     'gpios': 'gpios',
     'relays': 'user-relays'
+}
+
+
+STOP_CODES: dict = {
+    'SUCCESS': 0,
+    'ARGUMENTS': 1,
+    'EXECUTION_ERROR': -1,
 }
