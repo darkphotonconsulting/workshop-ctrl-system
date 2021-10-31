1443641b567a2ebf9ed0820a2a94ab8ab5eec74a
apsamuel <aaron.psamuel@gmail.com>
2021-10-31 15:59:41 -0400

    feat: toolkit updates

--- pylibs/utilx/toolkit.py
+++ pylibs/utilx/toolkit.py
@@ -1,7 +1,4 @@
 """ Utility functions
-
-Returns:
-    [type]: [description]
 """
 import os
 import sys
@@ -21,26 +18,86 @@ from typing import (
 
 @dataclass
 class EcoSystem:
-    packages: dict
-    modules: dict
-    classes: dict
+    """ Describes the headunit python ecosystem
+    """
+    packages: list
+    modules: list
+    classes: list
+
 
 @dataclass
 class Pkg:
+    """ A package within the ecosystem
+    """
     name: str
     ref: Any
     package: bool
+
+
 @dataclass 
 class Mod:
+    """ A module within the ecosystem
+    """
     name: str
     ref: Any
     package: bool
 
 @dataclass
 class Klass:
+    """ A class within the ecosystem
+    """
     name: str
     ref: Any
     funcs: list
+
+@dataclass
+class PkgReport:
+    """ Pkg Report
+    """
+    total_packages: int
+    missing_docs: list
+    has_docs: list
+    missing_all: list
+    has_all: list
+
+    def doc_coverage(self):
+        total = self.total_packages
+        return len(self.has_docs)/total*100
+        
+
+@dataclass
+class ModReport:
+    """ Mod Report
+    """
+    total_modules: int
+    total_loc: int
+    missing_docs: list
+    has_docs: list
+    missing_all: list
+    has_all: list
+
+
+    def doc_coverage(self):
+        total = self.total_modules
+        return len(self.has_docs)/total*100
+
+@dataclass
+class KlassReport:
+    total_classes: int
+    total_loc: int
+    missing_docs: list
+    has_docs: list
+
+
+    def doc_coverage(self):
+        total = self.total_classes
+        return len(self.has_docs)/total*100
+
+@dataclass
+class EcoSystemReport:
+    packages: PkgReport
+    modules: ModReport
+    classes: KlassReport
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = "/".join(CURRENT_DIR.split('/')[0:-2])
@@ -131,6 +188,15 @@ def packages(
 def package_names(
     ignore: list = None
 ) -> list:
+    """package_names a list of package names
+
+
+    Args:
+        ignore (list, optional): [description]. Defaults to None.
+
+    Returns:
+        list: [description]
+    """
     ignore = set(['pylibs']) + set(ignore) if ignore is not None else set(['pylibs'])
     return list(sorted(set([ name for finder, name, ispkg in walk_packages([PROJECT_ROOT]) if ispkg==True ]) - ignore))
 
@@ -138,7 +204,17 @@ def package_names(
 def modulesv2(
     package: ModuleInfo = None, 
     ignore: list = None
-):
+) -> list:
+    """modulesv2 a list of modules
+
+
+    Args:
+        package (ModuleInfo, optional): [description]. Defaults to None.
+        ignore (list, optional): [description]. Defaults to None.
+
+    Returns:
+        [type]: [description]
+    """
     modules = []
     if package is None:
         for loader, name, pkg in packages():
@@ -154,7 +230,16 @@ def modulesv2(
 def module_namesv2(
     package: ModuleInfo = None, 
     ignore: list = None
-):
+) -> list:
+    """module_namesv2 return list of module names
+
+    Args:
+        package (ModuleInfo, optional): a package module info object. Defaults to None.
+        ignore (list, optional): list of packages to ignore. Defaults to None.
+
+    Returns:
+        [type]: [description]
+    """
     modules = []
     if package is None:
         for loader, name, pkg in packages():
@@ -189,6 +274,15 @@ def modules(
     package: ModuleInfo = None,
     ignore: list = None
 ) -> list:
+    """modules list of module info objects
+
+    Args:
+        package (ModuleInfo, optional): a package module info object. Defaults to None.
+        ignore (list, optional): list of packages to ignore. Defaults to None.
+
+    Returns:
+        list: list of module info objects
+    """
     ignore = set(['pylibs']) + set(ignore) if ignore is not None else set(['pylibs'])
     if package is None:
         return [i for i in walk_packages([PROJECT_ROOT]) if i.ispkg==False and i.name not in ignore ]
@@ -201,6 +295,15 @@ def module_names(
     package: ModuleInfo,
     ignore: list = None
 ) -> list:
+    """module_names list of module names
+
+    Args:
+        package (ModuleInfo): [description]
+        ignore (list, optional): [description]. Defaults to None.
+
+    Returns:
+        list: [description]
+    """
     ignore = set(['pylibs']) + set(ignore) if ignore is not None else set(['pylibs'])
     if package is None:
         return list(sorted(set([ name for finder, name, ispkg in walk_packages([PROJECT_ROOT]) if ispkg==False ]) - ignore))
@@ -210,6 +313,14 @@ def module_names(
 def classes(
     module: ModuleInfo = None
 ) -> list:
+    """classes return list of tuples [(klassName, klass), ...]
+
+    Args:
+        module (ModuleInfo, optional): [description]. Defaults to None.
+
+    Returns:
+        list: [description]
+    """
     #modules = modules() if module is None else [i for i in modules()]
     klasses = []
     for loader, name, pkg in modules():
@@ -243,6 +354,16 @@ def classes(
 def class_names(
     module: ModuleInfo = None
 ) -> list:
+    """class_names return list of class names
+
+    
+
+    Args:
+        module (ModuleInfo, optional): module to list classes in. Defaults to None.
+
+    Returns:
+        list: list of class names
+    """
     klasses = []
     for loader, name, pkg in modules():
         #print(name)
@@ -278,56 +399,126 @@ def class_names(
 def functions(
     klass: object = None
 ) -> list:
-  funcs = []
-  for k, v in klass.__dict__.items():
-      if k.startswith('__'): continue
-      if type(v).__name__ == 'function': funcs.append(v)
-  return funcs     
+    """functions list of functions defined in class
+
+
+
+    Args:
+        klass (object, optional): the class object. Defaults to None.
+
+    Returns:
+        list: list of functions
+    """
+    funcs = []
+    for k, v in klass.__dict__.items():
+        if k.startswith('__'): continue
+        if type(v).__name__ == 'function': funcs.append(v)
+    return funcs     
 
 def function_names(
     klass: object = None
 ) -> list:
-  funcs = []
-  for k, v in klass.__dict__.items():
-      if k.startswith('__'): continue
-      if type(v).__name__ == 'function': funcs.append(v)
-  return funcs
+    """function_names [summary]
+
+    [extended_summary]
+
+    Args:
+        klass (object, optional): [description]. Defaults to None.
+
+    Returns:
+        list: [description]
+    """
+    funcs = []
+    for k, v in klass.__dict__.items():
+        if k.startswith('__'): continue
+        if type(v).__name__ == 'function': funcs.append(v)
+    return funcs
 
 
 
-def ecosystem() -> EcoSystem:
+def ecosystem(
+) -> EcoSystem:
+    """ecosystem return an EcoSystem object
+
     
-    eco = EcoSystem(
-        packages={
-            package.name: {
-                'ref': package.module_finder.find_module(package.name).load_module(package.name),
-                'package': package.ispkg,
-                
-            } for package in packages()
-            },
-        modules={
-            module.name: {
-                'ref': module.module_finder.find_module(module.name).load_module(module.name), 
-                'package': module.ispkg
-            } for module in modules()
-            },
-        classes={
-            
-        }
+
+    Returns:
+        EcoSystem: an EcoSystem object describing the code defined in pylibs
+    """
+    return EcoSystem(
+        packages=[
+            Pkg(
+                name=package.name,
+                ref=package.module_finder.find_module(package.name).load_module(package.name),
+                package=package.ispkg 
+            ) for package in packages()
+        ],
+        modules=[
+            Mod(
+                name=module.name,
+                ref=module.module_finder.find_module(module.name).load_module(module.name),
+                package=module.ispkg
+            ) for module in modules()
+        ],
+        classes=[
+            Klass(
+                name=klass_name,
+                ref=klass,
+                funcs=functions(klass)
+            ) for klass_name, klass in classesv2()
+        ]
     )
 
-    for mod in modules():
-        klasses = classesv2(module=mod)
-        for klass_name, klass in klasses:
-            eco.classes[klass_name] = dict(
-                ref=klass,
-                funcs=[]
+def report(
+    ecosys: EcoSystem = None
+) -> EcoSystemReport:
+        ecosys = ecosystem() if ecosys is None else ecosys
+        return EcoSystemReport(
+            packages = PkgReport(
+                total_packages=len(ecosys.packages),
+                missing_docs=[
+                    pkg.name for pkg in ecosys.packages if inspect.getdoc(pkg.ref) is None
+                ],
+                has_docs=[
+                    pkg.name for pkg in ecosys.packages if inspect.getdoc(pkg.ref) is not None
+                ],
+                missing_all=[
+                    pkg.name for pkg in ecosys.packages if '__all__' not in pkg.ref
+                ],
+                has_all=[
+                    pkg.name for pkg in ecosys.packages if '_all__' in pkg.ref
+                ]
+            ),
+            modules = ModReport(
+                total_modules=len(ecosys.modules),
+                total_loc=sum([
+                    len(inspect.getsourcelines(mod.ref)[0]) for mod in ecosys.modules if type(inspect.getsourcelines(mod.ref))==tuple and len(inspect.getsourcelines(mod.ref))==2
+                ]),
+                missing_docs=[
+                    mod.name for mod in ecosys.modules if inspect.getdoc(mod.ref) is None
+                ],
+                has_docs=[
+                    mod.name for mod in ecosys.modules if inspect.getdoc(mod.ref) is not None
+                ],
+                missing_all=[
+                    mod.name for mod in ecosys.modules if '__all__' not in mod.ref
+                ],
+                has_all=[
+                    mod.name for mod in ecosys.modules if '_all__' in mod.ref
+                ]
+            ), 
+            classes = KlassReport(
+                total_classes=len(ecosys.classes),
+                total_loc=sum([ len(inspect.getsourcelines(klass.ref)[0]) for klass in ecosys.classes if type(inspect.getsourcelines(klass.ref))==tuple and len(inspect.getsourcelines(klass.ref))==2 ]),
+                missing_docs=[
+                    klass.name for klass in ecosys.classes if inspect.getdoc(klass.ref) is None
+                ],
+                has_docs=[
+                    klass.name for klass in ecosys.classes if inspect.getdoc(klass.ref) is not None
+                ]
+                
             )
-            
-            funcs = functions(klass)
-            eco.classes[klass_name]['funcs'].extend(funcs)            
-    return eco
-
+        )
 
 
     
