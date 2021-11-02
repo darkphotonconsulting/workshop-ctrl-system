d10dbc09a7e5df83741e9c21d0eac65afa0b42bf
apsamuel <aaron.psamuel@gmail.com>
2021-11-01 21:35:19 -0400

    test: check in broken for assistance

--- pylibs/tests/test_pylibs.py
+++ pylibs/tests/test_pylibs.py
@@ -6,12 +6,19 @@ libs = "/".join(current_dir.split('/')[0:-2])
 sys.path.append(libs)
 #print(libs)
 
-from pylibs.utilx.toolkit import ecosystem
+from pylibs.utilx.toolkit import (
+    ecosystem, 
+    report
+)
 from pylibs.utilx.toolkit import (
     EcoSystem,
     Pkg,
     Mod,
-    Klass
+    Klass,
+    EcoSystemReport,
+    PkgReport,
+    ModReport,
+    KlassReport
 )
 
 PYLIBS_MODULES = [
@@ -31,13 +38,24 @@ PYLIBS_MODULES = [
 def eco():
     return ecosystem()
 
+@pytest.fixture 
+def ecoreport():
+    return report()
+
 def test_ecosystem_struct(eco):
     assert isinstance(eco, EcoSystem)
     assert isinstance(eco.modules, list)
     assert isinstance(eco.packages, list)
     assert isinstance(eco.classes, list)
     
-    
+def test_ecosystem_completeness(ecoreport, capsys):
+    sys.stdout.write(f"report type: {type(ecoreport)}")
+    assert isinstance(ecoreport, EcoSystemReport)
+    assert isinstance(ecoreport.packages, PkgReport)
+    #assert type(ecoreport)==EcoSystemReport
+    assert isinstance(ecoreport.modules, ModReport)
+    assert isinstance(ecoreport.classes, KlassReport)
+    #assert isinstance("", str)
 
 
     
