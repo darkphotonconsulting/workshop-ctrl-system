e12e7baece47bee0130b6e40b617a9e57df258a0
apsamuel <aaron.psamuel@gmail.com>
2021-11-06 15:26:16 -0400

    test: improve tests

--- pylibs/tests/test_pylibs.py
+++ pylibs/tests/test_pylibs.py
@@ -1,11 +1,26 @@
-import os
-import sys
-import pytest
-current_dir = os.path.dirname(os.path.abspath(__file__))
-libs = "/".join(current_dir.split('/')[0:-2])
-sys.path.append(libs)
-#print(libs)
+#import os
 
+from os.path import (
+    dirname,
+    abspath
+)
+import sys
+
+from sys import (
+    stdout,
+    stderr,
+    path,
+    modules
+)
+from random import (
+    randint
+)
+import pytest
+current_dir = dirname(abspath(__file__))
+libs = "/".join(current_dir.split('/')[0:-2])
+path.append(libs)
+#print(libs)
+import pylibs.utilx.toolkit
 from pylibs.utilx.toolkit import (
     ecosystem, 
     report
@@ -20,6 +35,8 @@ from pylibs.utilx.toolkit import (
     ModReport,
     KlassReport
 )
+
+DOC_COVERAGE = 75
 
 PYLIBS_MODULES = [
     'pylibs.arduino.programmer',
@@ -36,29 +53,73 @@ PYLIBS_MODULES = [
 
 @pytest.fixture
 def eco():
+    """eco returns ecosystem 
+    """
     return ecosystem()
 
 @pytest.fixture 
 def ecoreport():
+    """ecoreport return ecosystem report
+
+    
+
+    Returns:
+        EcoSystemReport: an eco system code report
+    """
     return report()
 
-def test_ecosystem_struct(eco):
+@pytest.fixture
+def eco_package(eco):
+    return eco.packages[randint(0, len( eco.packages)-1)]
+
+@pytest.fixture
+def eco_module(eco):
+    return eco.modules[randint(0, len( eco.modules)-1)]
+
+@pytest.fixture
+def eco_class(eco):
+    return eco.classes[randint(0, len( eco.classes)-1)]
+
+def test_eco_structure(eco):
+    """test_eco_structure test eco system object structure
+
+    
+
+    Args:
+        eco (EcoSystem): validates an EcoSystem structure
+    """
     assert isinstance(eco, EcoSystem)
     assert isinstance(eco.modules, list)
     assert isinstance(eco.packages, list)
     assert isinstance(eco.classes, list)
     
-def test_ecosystem_completeness(ecoreport, capsys):
+def test_eco_report(ecoreport, capsys):
     sys.stdout.write(f"report type: {type(ecoreport)}")
-    assert isinstance(ecoreport, EcoSystemReport)
-    assert isinstance(ecoreport.packages, PkgReport)
-    #assert type(ecoreport)==EcoSystemReport
-    assert isinstance(ecoreport.modules, ModReport)
-    assert isinstance(ecoreport.classes, KlassReport)
+    assert isinstance(ecoreport, pylibs.utilx.toolkit.EcoSystemReport)
+    assert isinstance(ecoreport.packages, pylibs.utilx.toolkit.PkgReport)
+    # #assert type(ecoreport)==EcoSystemReport
+    assert isinstance(ecoreport.modules, pylibs.utilx.toolkit.ModReport)
+    assert isinstance(ecoreport.classes, pylibs.utilx.toolkit.KlassReport)
     #assert isinstance("", str)
 
+def test_eco_code_quality(ecoreport, capsys):
+    assert ecoreport.packages.doc_coverage() > DOC_COVERAGE
+    stdout.write(f"package doc coverage: {ecoreport.packages.doc_coverage()} %")
+    assert ecoreport.modules.doc_coverage() > DOC_COVERAGE
+    stdout.write(f"module doc coverage: {ecoreport.modules.doc_coverage()} %")
+    assert ecoreport.classes.doc_coverage() > DOC_COVERAGE
+    stdout.write(f"class doc coverage: {ecoreport.classes.doc_coverage()} %")
 
-    
+def test_random_eco_package(eco_package):
+    assert isinstance(eco_package, pylibs.utilx.toolkit.Pkg)
+
+def test_random_eco_module(eco_module):
+    assert isinstance(eco_module, pylibs.utilx.toolkit.Mod)
+
+def test_random_eco_class(eco_class):
+    assert isinstance(eco_class, pylibs.utilx.toolkit.Klass)
+
+
 def test_imports(capsys):
     """test_imports Test imports under the pylibs module tree
     """
@@ -69,7 +130,7 @@ def test_imports(capsys):
             globals()[module] = __import__(module)
 
         assert all(
-            module_path in [m for m in sys.modules.keys() if m.startswith('pylibs')] for module_path in PYLIBS_MODULES
+            module_path in [m for m in modules.keys() if m.startswith('pylibs')] for module_path in PYLIBS_MODULES
         )
     except ImportError as error_message:
         print('error...')
